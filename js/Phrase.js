bce6ffa3d8391cab39be386b4323b4a23a74a449
crisdevs <crisdevs@github.com>
2021-03-31 22:39:13 -0400

    Added Phrase class, constructor and addPhraseToDisplay

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,29 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+    constructor (phrase){
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay = () =>{
+        const phraseChar = this.phrase.split("");
+        const phraseUL = document.querySelector("#phrase ul");
+
+        phraseChar.forEach(letter =>{
+            const li = document.createElement("li");
+            li.textContent = letter;
+
+            if(letter !== " "){
+                li.className = `hide letter ${letter}`;
+            }
+            else{
+                li.className = "space";
+            }
+
+            phraseUL.appendChild(li);
+        });
+
+
+    }
+}
